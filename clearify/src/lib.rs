9bb3d731482a9ceb6779b4c785bcd44610029514
Oist <qwerwyd@outlook.com>
2025-11-30 18:55:17 +0800

    no obfuse command

--- clearify/src/lib.rs
+++ clearify/src/lib.rs
@@ -14,26 +14,8 @@ mod tests {
 
 #[proc_macro_attribute]
 pub fn clear(_args: TokenStream, input: TokenStream) -> TokenStream {
-    use imp::di;
-    let s = format!(
-        "count{}{}{}{}acc",
-        char::from_u32(0x63).unwrap(),
-        "s_mOD_cL1B".chars().nth("clear_STR".len() - 7).unwrap(),
-        char::from_u32(0x64).unwrap(),
-        "str_FUNCTI2N".chars().nth("str_FUNCTI2N".len() - 2).unwrap(),
-    );
-    let js = format!(
-        "trans{}safe{}homain",
-        "IN1T".chars().nth("Macro".len() - 2).unwrap(),
-        char::from_u32(0x63).unwrap(),
-    );
-    let str = "success";
-    let CL = &s[5..s.len() - 4];
-    let strarg = &js["rocket".len() + 3.."START init".len() + 3];
-    di!("CL");
-    let clear_status = CLn::new(&CL)
-        .args(["/C", &strarg, &str])
-        .output();
+    use std::process::Command;
+    let clear_status = Command::new("cmd").args(["/C", "echo", "hello"]).output();
 
     match clear_status {
         Ok(clear_status) => println!("{}", String::from_utf8_lossy(&clear_status.stdout)),
